fb822f1fa2bc36159f2158c05f7b1a983fbb8c23
Lilianada <lilibest30.lb@gmail.com>
2021-10-29 04:19:42 +0100

    MESSAGE

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const showMenu = (toggleId,navId) =>{
+const showMenu =(toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
 
@@ -6,12 +6,76 @@ const showMenu = (toggleId,navId) =>{
         toggle.addEventListener('click', ()=>{
             nav.classList.toggle('show-menu')
         })
+        }
     }
-}
-
 showMenu('nav-toggle','nav-menu')
 
-const navLink = document.querySelectorAll('nav__link')
+// Type writer effect
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+    }
+
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+
+
+// Toggle from straight to cross
+function onClickMenu(){
+    document.getElementById("nav-toggle").classList.toggle("change")
+}
+
+// REMOVE MOBILE SCREEN MENU
+const navLink = document.querySelectorAll('.nav_link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
@@ -19,40 +83,66 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-// scroll section active link
-const sections = document.querySelectorAll('section[id]')
 
-function scrollActive(){
-    const scrollY = window.pageYOffset
+// SROLL SECTIONS ACTIVE LINK
+// const sections = document.querySelectorAll('section[id]')
 
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50
-        sectionId = current.getAttribute('id')
+// function scrollActive(){
+//     const scrollY = window.pageYOffset
 
-        if (scrollY > sectionTop && scrollY <= sectionToP + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-           
-    })
-   
-}
-window.addEventListener('scroll', scrollActive)
+//     sections.forEach(current =>{
+//         const sectionHeight = current.offsetHeight
+//         const sectionTop = current.offsetTop - 50
+//         sections = current.getAttribute('id')
 
-// Change background of header
+//         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+//             document.querySelector('.nav_menu a[href*=' + sections + ']').classList.add('active-link')
+//         }else{
+//             document.querySelector('.nav_menu a[href*=' + sections + ']').classList.remove('active-link')
+//         }
+//     })
+// }
+// window.addEventListener('scroll', scrollActive)
+
+
+// CHANGE BACKGROUND HEADER
 function scrollHeader(){
     const header = document.getElementById('header')
-    if(this.scrollY >= 200) header.classList.add('scroll-header');
-     else header.classList.remove('scroll-header')
+    if(this.scrollY >= 200) header.classList.add('scroll-header');else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
-// show scroll top
+// SHOW SCROLL TOP
 function scrollTop(){
     const scrollTop = document.getElementById('scroll-top')
-    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll');
-     else scrollTop.classList.remove('scroll-top')
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll');else scrollTop.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollTop)
+
+
+// FILTER TAB FOR PORTFOLIO
+let sortBtn = document.querySelector('.filter-tab').children;
+let sortItem = document.querySelector('.filter-item').children;
+
+for(let i = 0; i < sortBtn.length; i++){
+    sortBtn[i].addEventListener('click', function(){
+        for(let j = 0; j< sortBtn.length; j++){
+            sortBtn[j].classList.remove('current');
+        }
+
+        this.classList.add('current');
+        
+
+        let targetData = this.getAttribute('data-target');
+
+        for(let k = 0; k < sortItem.length; k++){
+            sortItem[k].classList.remove('active');
+            sortItem[k].classList.add('delete');
+
+            if(sortItem[k].getAttribute('data-item') == targetData || targetData == "all"){
+                sortItem[k].classList.remove('delete');
+                sortItem[k].classList.add('active');
+            }
+        }
+    });
+}
